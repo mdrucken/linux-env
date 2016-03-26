bd3d2ef26e861801426ce3d4e730bc66e4d93fa1
Marc Druckenmueller <marc.druckenmueller@gmail.com>
2016-03-26 09:02:10 +0100

    .

--- git.sh
+++ git.sh
@@ -2,5 +2,5 @@
 git config --global push.default simple
 git config --global user.name "Marc Druckenmueller"
 git config --global user.email marc.druckenmueller@gmail.com
-git config credential.https://github.username marc.druckenmueller@gmail.com
+git config credential.https://github.com.username marc.druckenmueller@gmail.com
 git config credential.helper 'cache --timeout=3600'
